7d6277996ffbcf03f1f77aece375de5f99d2cb28
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2023-11-23 02:13:58 +0800

    Update hw04.c

--- homework/hw04.c
+++ homework/hw04.c
@@ -123,4 +123,18 @@ int main(void)
     return 0;
 }
 
-//https://wm5.nccu.edu.tw/base/10001/course/10027586/content/asgmt04/index.html
+//introduction
+//Your task is to develop a program to determine if a given matrix is testable or not.
+//If the matrix is non-testable and only required to change one bit to fulfill the condition, your program should be able to point out which bit should be changed.
+//Otherwise, the matrix should be classified as a corrupt one.
+
+//input
+//The input file will contain one or more test cases.
+//The first line of each test case contains an integer n (n < 100), representing the size of the square matrix. On the next n lines, there will be n integers per line.
+//Note. No other integers than 0 and 1 will occur in the matrix. Input will be terminated by a value of 0 for n.
+
+//output
+//For each matrix in the input file, print one line.
+//If the matrix is testable, print 'OK'.
+//If the testable boolean matrix can be established by changing one bit, print 'Change bit (i,j)' where i is the row and j is the column of the bit to be changed.
+//Otherwise, print 'Corrupt'.
